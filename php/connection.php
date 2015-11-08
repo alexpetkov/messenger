7192be39e2a7a3ce9b4322f6e607035aadadbb64
alexpetkov <aleksandarpetkov@gmail.com>
2015-11-08 18:07:21 +0200

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -1,3 +1,3 @@
 <?php 
-	$db = new PDO('mysql:host=localhost;dbname=studiose_sms;charset=utf8', 'studiose_sms', 'message123');
+	$db = new PDO('mysql:host=localhost;dbname=******;charset=utf8', '******', '******');
 ?>
